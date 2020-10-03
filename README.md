693e8480b38994c4c7e71b8565ca4a69f0d90b90
praise-laurine <praiselaurine10@gmail.com>
2020-10-04 00:08:25 +0300

    update changes

---
+++ README.md
@@ -0,0 +1,36 @@
+### Name
+AKAN NAMING SYSTEM
+
+## Description 
+This application allows user to enter their date <br>of birth, century and gender to display their akan name.
+
+# Created By:
+Laurine Praise.
+
+# Set Up & Instalation Guide
+
+* After my repository's name, Select Clone/download.
+* In the Clone with HTTPs , select the repository to copy it in the Terminal.
+* Open the terminal.
+* Select Where you want to clone the project.
+* Type git clone then paste the HTTPs link you copied.
+* Click Enter. And your clone will be created.
+
+#
+
+# Technologies Used
+* CSS
+* HTML
+* BootstrapYour local clone will be created. 
+* Javascript
+
+# Contact Details
+praiselaurine10@gmail.com
+
+# License & Copyright
+Copyright (c) 2020 Laurine Praise
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
---
+++ js/script.js
@@ -0,0 +1,117 @@
+var century, year, month, dayOfMonth, dayOfWeek, day;
+
+function getInput(){
+  century = parseInt(document.getElementById("century").value);
+  year = parseInt(document.getElementById("year").value);
+  month = parseInt(document.getElementById("month").value);
+  dayOfMonth = parseInt(document.getElementById("monthday").value);
+
+
+  if(century == ""){
+    alert("Input the correct gender");
+    return false;
+  }else if (year == ""){
+    alert("Input the correct year");
+    return false;
+  }else if (month == ""){
+    alert("Input the correct month");
+    return false;
+  }else if(dayOfMonth == ""){
+    alert("input the correct date");
+    return false;
+  }
+}
+
+function calculateDay(){
+    getInput();
+    dayOfWeek = ((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + dayOfMonth) % 7) -1;
+    console.log(dayOfWeek);
+    return (Math.floor(dayOfWeek));
+    if (dayOfWeek < 0) {
+      dayOfWeek = dayOfWeek * -1;
+    }
+    else if (dayOfWeek > 0) {
+      return dayOfWeek;
+    }
+}
+
+
+ function checkDayOfWeek(){
+     day = calculateDay();
+      checkGender();
+      console.log("The function runs");
+}
+
+
+let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let maleNames = ["Kwasi!", "Kwadwo!", "Kwabena!", "Kwaku!", "Yaw!", "Kofi!", "Kwame!"];
+
+
+function checkGender(){
+  var gen = document.getElementsByName("rads");
+  if(gen[0].checked == true){
+      var gender = "male";
+  }else if(gen[1].checked == true){
+      var gender = "female";
+  }else {
+    console.log("pass");
+  }
+    switch(gender){
+        case gender = "male":
+              switch(day){
+                case (0 || -0):
+                  document.getElementById("result").innerHTML = "The day is on a Sunday." + "  " + "Your akan name is " + maleNames[0];
+                break;
+                case (1 || -1):
+                  document.getElementById("result").innerHTML = "The day is on a Monday." + " " + "Your akan name is " + maleNames[1];
+                break;
+                case (2 || -2):
+                  document.getElementById("result").innerHTML = "The day is on a Tuesday." + " " + "Your akan name is " + maleNames[2];
+                break;
+                case (3 || -3):
+                  document.getElementById("result").innerHTML = "The day is on a Wednesday." + " " + "Your akan name is "+ maleNames[3];
+                break;
+                case (4 || -4):
+                  document.getElementById("result").innerHTML = "The day is on a Thursday." + " " + "Your akan name is " + maleNames[4];
+                break;
+                case (5 || -5):
+                  document.getElementById("result").innerHTML = "The day is on a Friday." + " " + "Your akan name is " + maleNames[5];
+                break;
+                case (6 || -6):
+                  document.getElementById("result").innerHTML = "The day is on a Saturday." + " " + "Your akan name is " + maleNames[6];
+                break;
+                default:
+    
+              }
+        break;
+        case gender = "female":
+                switch(day){
+                  case 0 || -0:
+                    document.getElementById("result").innerHTML = "The day is on a Sunday." + "  " + "Your akan name is  Akosua!";
+                  break;
+                  case 1 || -1:
+                    document.getElementById("result").innerHTML = "The day is on a Monday." + " " + "Your akan name is Adwoa!";
+                  break;
+                  case 2 || -2:
+                    document.getElementById("result").innerHTML = "The day is on a Tuesday." + " " + "Your akan name is Abenaa!";
+                  break;
+                  case 3 || -3:
+                    document.getElementById("result").innerHTML = "The day is on a Wednesday." + " " + "Your akan name is Akua!";
+                  break;
+                  case 4 || -4:
+                    document.getElementById("result").innerHTML = "The day is on a Thursday." + " " + "Your akan name is Yaa!";
+                  break;
+                  case 5 || -5:
+                    document.getElementById("result").innerHTML = "The day is on a Friday." + " " + "Your akan name is Afua";
+                  break;
+                  case 6 || -6:
+                    document.getElementById("result").innerHTML = "The day is on a Saturday." + " " + "Your akan name is Ama!";
+                  break;
+
+              }
+              break
+              default: {
+                alert("Input the all data Please!")
+              }
+    }
+}
