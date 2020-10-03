808a3a5101ab9e24e1d5e547c58bc6c6ddda1f8e
praise-laurine <praiselaurine10@gmail.com>
2020-10-03 16:48:51 +0300

    added  variables

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ let femaleNames=[" Akosua", "Adwoa", "Abenaa", " Akua", "Yaa","Afua", " Ama"];
 
 function checkDayOfWeek() {
   let year =parseInt(document.getElementById("year").value);
-  let year =parseInt(document.getElementById("month").value);
-  let year =parseInt(document.getElementById("century").value);
-  let year =parseInt(document.getElementById("monthDay").value);
-  let year =parseInt(document.getElementById("rad1").value);
+  let month =parseInt(document.getElementById("month").value);
+  let century =parseInt(document.getElementById("century").value);
+  let monthDay =parseInt(document.getElementById("monthDay").value);
+  let genders =parseInt(document.getElementById("rad1").value);
 
   let YY = century;
+  let MM = month;
+  let CC = century;
+  let MD = monthDay;
+  let genders = rad1;
 
 
 
