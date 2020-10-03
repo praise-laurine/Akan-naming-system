b970191fea28281a5edbc6c66eaac56709c79c80
praise-laurine <praiselaurine10@gmail.com>
2020-10-03 16:41:05 +0300

    add function

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 let maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", " Kofi", "Kwame"];
 let femaleNames=[" Akosua", "Adwoa", "Abenaa", " Akua", "Yaa","Afua", " Ama"];
+
+function checkDayOfWeek() {
+  let year =parseInt(document.getElementById("year").value);
+  let year =parseInt(document.getElementById("month").value);
+  let year =parseInt(document.getElementById("century").value);
+  let year =parseInt(document.getElementById("monthDay").value);
+  let year =parseInt(document.getElementById("rad1").value);
+
+  let YY = century;
+
+
+
+
+
+
+}
