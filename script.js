02a4f1124ff964df4942e6705493ff2ae36c9149
praise-laurine <praiselaurine10@gmail.com>
2020-10-03 16:06:54 +0300

    made changes

--- script.js
+++
@@ -1,8 +0,0 @@
-var century, year, month, dayOfMonth, dayOfWeek, day;
-//Get input
-function getInput(){
-  century = parseInt(document.getElementById("century").value);
-  year = parseInt(document.getElementById("year").value);
-  month = parseInt(document.getElementById("month").value);
-  dayOfMonth = parseInt(document.getElementById("monthDay").value);
-}
